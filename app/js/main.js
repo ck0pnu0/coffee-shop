61f6db8f387e1dc6e2081dde5551bc6b52e63f7a
ck0pnu0 <georgi.dimitrov89@gmail.com>
2016-09-27 08:02:06 +0300

    desktop progress

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,14 @@
 	var $doc = $(document);	
 
 	$doc.ready(function() {
-		
+		//scrollTop
+		$('.scroll-top').on('click', function(e) {
+			e.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: 0
+			}, 600);
+		});
 	});
 
 })(jQuery, window, document);
